06136b3fb16d6f97a4fdf29f62ceab14eac208c0
Scodia619 <billyjoe2701@gmail.com>
2022-06-19 03:27:17 +0100

    v1.2
    Sorted the starting CSS and starting to fiddle with API now

--- index.js
+++
@@ -1,7 +0,0 @@
-var arr = ["1", "2","3","4"];
-console.log(arr);
-
-i = 0
-for(i=0; i!=arr.length; i++){
-    console.log(arr[i]);
-}
--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+//Trying out adding more divs to the parent
+//getting the parent element
 const container = document.getElementById("container");
-var newDiv = document.createElement("div");
-var arr = ["1","1","1"];
+//array for testing
+const arr = ["1","1","1"];
 
-arr.forEach(function(element){
-    newDiv.innerHTML = element;
-    container.appendChild(newDiv)
-});
+//array.map runs through all the element of the arr array much like forEach would
+arr.map(x => {
+    const newDiv = document.createElement("div");
+    newDiv.className = "item";
+    newDiv.innerHTML += x;
+    container.append(newDiv)
+  });
+
+//changing font colour based on rating
+//setting the variable
+const rating = 9;
+
+//Conditional statements to decide what colour the font will be
+if(rating<4){
+  document.getElementById("film-rating").style.color = "red";
+}else if(rating>7){
+  console.log("test")
+  document.getElementById("film-rating").style.color = "#39FF14";
+}else{
+  document.getElementById("film-rating").style.color = "yellow";
+}
+
+//testing axios
+axios.get("https://api.themoviedb.org/3/movie/555?api_key=1c191405789eb90f303f6f758c15e1eb&language=en-US")
+.then(response => {
+  console.log(response.data);
+  let info = response.data;
+  console.log(info.poster_path)
+})
+.catch(error => console.error(error));
