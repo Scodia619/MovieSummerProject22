14f0b8105e7670bf33efaaaa6dec9abbf6c710cd
Scodia619 <billyjoe2701@gmail.com>
2022-06-28 21:13:49 +0100

    v1.4
    Getting and displaying all data for basic film genres is achieved. Next steps are to separate genres and adding an add to watchlist button

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ arr.map(x => {
   console.log(response.data);
   let info = response.data;
   for(let i=0; i < 5; i++){
+    //need to get image display working and then finish seperate them based on genre
+    //need to setup database and get rating and then colour code the rating if i have watched it
     console.log("https://image.tmdb.org/t/p/w500" + info.results[i].poster_path)
     container.innerHTML += `<div class="item">
-    <img src="https://image.tmdb.org/t/p/original"` + info.results[i].poster_path +` alt="`+ info.results[i].title +`">
+    <img src="https://image.tmdb.org/t/p/original` + info.results[i].poster_path +`" alt="`+ info.results[i].title +`">
     <div class="film-info ">
         <p class="film-title">` + info.results[i].title +`</p>
         <p class="film-rating">9/10</p>
     </div>
     <div class="film-description-holder">
-        <p class="film-description" id="four">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, esse possimus illum vero ut perferendis architecto reiciendis voluptates sint, nisi, eos ullam? Cupiditate iure quas, quibusdam voluptas suscipit beatae saepe.</p>
+        <p class="film-description">` + info.results[i].overview +`</p>
     </div>
 </div>`
   }
@@ -38,5 +40,9 @@ if(rating<4){
   console.log("test")
   document.getElementById("film-rating").style.color = "#39FF14";
 }else{
-  document.getElementsByClassName("film-rating")[0].style.color = "yellow";
+  document.getElementsByClassName("film-rating")[1].style.color = "yellow";
+}
+
+function add(){
+  console.log("add")
 }
