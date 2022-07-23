980b36e36c40367d2559767224ca9fb9f107c3e9
Scodia619 <billyjoe2701@gmail.com>
2022-07-24 00:35:33 +0100

    React V1.1
    Trying Out React.JS

---
+++ movie-summer-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import MovieCardItem from "./components/MovieCardItem";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World!</p>
+      <MovieCardItem></MovieCardItem>
+    </div>
+  );
+}
+
+export default App;
